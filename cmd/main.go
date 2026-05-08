0afbef0650e7a74b6e4b3c8dc29a606c1d44af60
nathanim1919 <nathanim2tadele@gmail.com>
2026-05-08 23:12:37 +0300

    feat: add graceful shutdown handling for HTTP server

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+    "context"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 
 	"global-event-feed/config"
 	"global-event-feed/internal/handler"
@@ -29,6 +34,50 @@ func main() {
 	r := chi.NewRouter()
 	eventHandler.RegisterRoutes(r)
 
-	log.Printf("Server running on port %s", cfg.Port)
-	http.ListenAndServe(":"+cfg.Port, r)
+	// log.Printf("Server running on port %s", cfg.Port)
+	server := &http.Server{
+		Addr: ":" + cfg.Port,
+		Handler: r,
+	}
+
+
+    // Run Server in Goroutine
+    go func(){
+      log.Println("Server started on :"+cfg.Port)
+
+      if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+          log.Fatalf("server error: %v", err)
+      }
+    }()
+
+
+    // Channel to receive OS signals
+    stop := make(chan os.Signal, 1)
+
+
+    // Listen for shutdown signals
+    signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+
+    // Block until signal received
+    <-stop
+
+
+    log.Println("Shutting down server ...")
+
+    // Timeout for gracefull shutdown
+    ctx, cancle := context.WithTimeout(
+    	context.Background(),
+        5*time.Second,
+    )
+    defer cancle()
+
+
+    // Graceful shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("shutdown failed: %v", err)
+	}
+
+
+	log.Println("Server exited cleanly")
 }
